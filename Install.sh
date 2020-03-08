292fe92afc895aa8e34aab2d5e19ce40057077d7
silverlogix <cameron.c.ait@gmail.com>
2020-03-08 01:44:23 -0600

    bash_aliases

---
+++ Install.sh
@@ -0,0 +1,24 @@
+#Do not use yet
+#Mar 08, 2020, 01:39:12
+
+sudo apt install 
+
+preload
+ksystemlog
+exfat-fuse exfat-utils
+
+sudo preload start
+
+sudo apt-get remove --purge qlipper
+sudo apt-get remove --purge libreo*
+sudo apt-get remove --purge k3b
+
+sudo apt clean
+sudo apt autoremove
+
+
+
+sudo apt-get install git build-essential kernel-package fakeroot libncurses5-dev libssl-dev ccache bison flex autoconf automake autotools-dev dkms libelf-dev libiberty-dev libpci-dev libudev-dev zlib1g-dev
+
+
+
