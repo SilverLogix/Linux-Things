9880cab7058cae1530f3d2e971de35ea34822154
Faxanidu <cameron.c.ait@gmail.com>
2020-03-12 01:39:21 -0500

    moar apps

--- Install.sh
+++ Install.sh
@@ -1,25 +1,27 @@
-#Do not use yet!!!
-#Using for an auto install script menu later!
-#Mar 08, 2020, 01:39:12
+# Do not use yet!!!
+# Using for an auto install script menu later!
+# Mar 08, 2020, 01:39:12
 
 sudo apt install 
 
-preload
-ksystemlog
-exfat-fuse exfat-utils
-qdirstat
+preload					# Fast start, much wow
+ksystemlog				# God of log
+exfat-fuse exfat-utils  # For mounting exfat
+qdirstat				# Find things taking up space!
+
 
 sudo preload start
 
+# Why would i want these
 sudo apt-get remove --purge qlipper
 sudo apt-get remove --purge libreo*
-sudo apt-get remove --purge k3b
+
 
 sudo apt clean
 sudo apt autoremove
 
 
-
+# For compiling things!
 sudo apt-get install git build-essential kernel-package fakeroot libncurses5-dev libssl-dev ccache bison flex autoconf automake autotools-dev dkms libelf-dev libiberty-dev libpci-dev libudev-dev zlib1g-dev
 
 
